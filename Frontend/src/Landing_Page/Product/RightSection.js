f12f6f73817c4c1a0fada3d2006f2c8d2d0f5109
Moksh Patel <144706274+MokshPatel05@users.noreply.github.com>
2024-11-24 23:31:19 +0530

    Completed RightImage Section

--- Frontend/src/Landing_Page/Product/RightSection.js
+++ Frontend/src/Landing_Page/Product/RightSection.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React from "react";
 
-function RightSection() {
-    return ( 
-        <h1>Right  Section</h1>
-     );
+function RightSection({
+  imageURL,
+  productName,
+  productDescription,
+  learnMore,
+  linkName,
+}) {
+  return (
+    <div className="container">
+      <div className="row mt-5" style={{ paddingRight: "10rem" }}>
+        <div className="col-6 mt-5" style={{ padding: "4rem 0 0 10rem" }}>
+          <h1 style={{ margin: "0 0 2rem 0" }}>{productName}</h1>
+          <p
+            style={{
+              padding: "0 7rem 0 0",
+              lineHeight: "1.9rem",
+              fontFamily: "sans-serif",
+            }}>
+            {productDescription}
+          </p>
+          <div className="mt-3">
+            <a href={learnMore}>
+              {linkName}&nbsp; →
+            </a>
+          </div>
+        </div>
+        <div className="col-6">
+          <img src={imageURL} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default RightSection;
