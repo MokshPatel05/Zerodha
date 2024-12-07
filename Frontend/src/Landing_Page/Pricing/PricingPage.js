15cda632d1dc130a5b31c38c927faa0308758683
Moksh <patelmoksh2023@gmail.com>
2024-12-07 22:28:07 +0530

    modified Pricing section.

--- Frontend/src/Landing_Page/Pricing/PricingPage.js
+++ Frontend/src/Landing_Page/Pricing/PricingPage.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Hero from "./Hero";
 import Brokerage from "./Brokerage";
-import Navbar from "../Navbar";
-import Footer from "../Footer";
 
 function PricingPage() {
   return (
